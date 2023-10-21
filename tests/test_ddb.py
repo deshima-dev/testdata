501f0aeb800e4b9f2c838bcf1bd4453b906ae008
Akio Taniguchi <taniguchi@a.phys.nagoya-u.ac.jp>
2023-10-21 11:52:02 +0000

    #15 Update test for DDBs

--- tests/test_ddb.py
+++ tests/test_ddb.py
@@ -9,12 +9,12 @@ from pytest import mark
 
 # constants
 DDB_DIR = Path(__file__).parents[1] / "ddb"
-DDB_ALL = list(DDB_DIR.glob("*.fits.gz"))
+DDB_ALL = map(Path, DDB_DIR.glob("*.fits.gz"))
 
 
 # test functions
 @mark.parametrize("ddb", DDB_ALL)
 def test_open(ddb: Path) -> None:
-    """Test existence and opening of DDBs."""
+    """Test existence and opening of DDB."""
     with fits.open(ddb):
         pass
