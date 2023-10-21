e49797a406eb21ccf684e96feb61439493e7a4ff
Akio Taniguchi <taniguchi@a.phys.nagoya-u.ac.jp>
2023-10-21 11:52:16 +0000

    #15 Update test for DEMSs

--- tests/test_dems.py
+++ tests/test_dems.py
@@ -3,17 +3,17 @@ from pathlib import Path
 
 
 # dependencies
-import xarray as xr
+from decode import io
 from pytest import mark
 
 
 # constants
 DEMS_DIR = Path(__file__).parents[1] / "dems"
-DEMS_ALL = list(DEMS_DIR.glob("*.nc.gz"))
+DEMS_ALL = map(Path, DEMS_DIR.glob("*.nc.gz"))
 
 
 # test functions
 @mark.parametrize("dems", DEMS_ALL)
 def test_open(dems: Path) -> None:
-    """Test existence and opening of DEMSs."""
-    xr.open_dataarray(dems)
+    """Test existence and opening of DEMS."""
+    io.open_dems(dems)
